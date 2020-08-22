ff25a607136f88bec02e95353d9335a9b1e8b9e8
Enrique <enrique.kreuk@icloud.com>
2020-08-22 11:45:20 +0200

    Added default selected

--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -7,7 +7,12 @@
             <button v-else-if="isFetchingInfo" disabled>Loading..</button>
             <button v-else-if="!canDownload" disabled>Download</button>
         </div>
+        <div>
+            <button @click="selectAudioOnly" v-if="audioOnly" class="audio-button" id="audio-button-active">Audio</button>
+            <button @click="selectAudioOnly" v-else class="audio-button">Audio</button>
+        </div>
         <select v-if="info" v-model="options.quality">
+            <option disabled selected>Quality</option>
             <option
             v-for="(format, index) in getFormats"
             :key="index"
@@ -15,7 +20,7 @@
             >{{format.format_note}}</option>
         </select>
         <select v-else v-model="options.quality">
-            <option>Quality</option>
+            <option disabled selected>Quality</option>
         </select>
         <!-- <input type="checkbox" v-model="options.playlist" /> -->
         <!-- <label>Playlist</label> -->
@@ -32,11 +37,12 @@ export default {
             options : {
                 playlist: false,
                 url : "",
-                quality: null
+                quality: "Quality"
             },
             isDownloading: false,
             canDownload: false,
-            isFetchingInfo: false
+            isFetchingInfo: false,
+            audioOnly: false
         });
     },
     methods: {
@@ -54,21 +60,35 @@ export default {
             if(this.options.url === "")
                 return;
 
-            this.isFetchingInfo = true;
-            let reg = this.options.url.match(/v=([0-9a-zA-Z$-_.+!*'(),]+)/);
-            const id = reg[1];
+            try{
+                this.isFetchingInfo = true;
+                let reg = this.options.url.match(/v=([0-9a-zA-Z$-_.+!*'(),]+)/);
+                const id = reg[1];
 
-            axios.get(`/info/video/${id}`)
-            .then(result => result.data)
-            .then(result => {
-                this.info = result;
+                axios.get(`/info/video/${id}`)
+                .then(result => result.data)
+                .then(result => {
+                    this.info = result;
+                    this.isFetchingInfo = false;
+                    this.canDownload = true;
+                })
+                .catch(error => {
+                    console.log(error);
+                    this.canDownload = true;
+                    this.isFetchingInfo = false;
+                });
+            }
+            catch(error){
+                alert('invalid youtube url');
                 this.isFetchingInfo = false;
-                this.canDownload = true;
-            })
-            .catch(error => {
-                console.log(error);
-                this.canDownload = true;
-            });
+                console.warn(error);
+            }
+        },
+        selectAudioOnly(){
+            if(this.audioOnly)
+                this.audioOnly = false;
+            else
+                this.audioOnly = true;
         }
     },
     computed: {
@@ -76,14 +96,17 @@ export default {
             let ar = [];
 
             this.info.formats.forEach(el => {
-                if(el.ext == "mp4" && el.format_note != 'tiny'){
-                    ar.push(el);
+                if(this.audioOnly){
+                    if(el.ext == "mp4" || el.ext == "webm" && el.vcodec == "none"){
+                        ar.push(el);
+                    }
+                }
+                else{
+                    if(el.ext == "mp4" && el.acodec != "none"){
+                        ar.push(el);
+                    }
                 }
             });
-
-            // ar = ar.filter((item, index) => {
-            //     return ar.indexOf(item) === index
-            // });
 
             return ar;
         }
@@ -99,11 +122,13 @@ header
     background: #34495e;
     width: 100%;
     top: 0;
+    min-height: 50px;
 }
 input[type="url"]
 {
     padding: 10px;
     width: 400px;
+    outline: none;
 }
 select
 {
@@ -118,5 +143,20 @@ button
     margin-left: -1px;
     outline: none;
     min-width: 127px;
+}
+.audio-button
+{
+    margin-top: 1px;
+    min-width: 70px;
+    user-select: none;
+}
+#audio-button-active
+{
+    -webkit-box-shadow: inset -1px 2px 24px 2px rgba(0,0,0,0.2);
+    -moz-box-shadow: inset -1px 2px 24px 2px rgba(0,0,0,0.2);
+    box-shadow: inset -1px 2px 5px 2px rgba(0,0,0,0.2);
+    background: #2ecc71;
+    color: white;
+
 }
 </style>
