7dd449126b0bb7653865bb64a9827b0e0ae2415e
Enrique <enrique.kreuk@icloud.com>
2020-08-22 11:45:31 +0200

    added watch route

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Watch from '../views/Watch.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ Vue.use(VueRouter)
     name: 'Home',
     component: Home
   },
+  {
+    path: '/watch/:id',
+    name: 'Watch',
+    component: Watch
+  }
 ]
 
 const router = new VueRouter({
