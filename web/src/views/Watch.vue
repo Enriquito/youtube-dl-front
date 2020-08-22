de44273c2d9a43608a6e9199a11096fbcdb317d2
Enrique <enrique.kreuk@icloud.com>
2020-08-22 11:45:43 +0200

    added watch vieuw

---
+++ web/src/views/Watch.vue
@@ -0,0 +1,61 @@
+<template>
+    <main>
+        <header>
+            <router-link :to="{name:'Home'}">
+                <img src="@/assets/icons/arrowleft.svg" alt="Back" />
+            </router-link>
+        </header>
+        <div class="d-flex justify-content-center" style="margin-top: 50px">
+            <div style="width: 1024px;">
+                <video :src="`${serverUrl}/media/${this.item.id}.${this.item.extention}`" controls />
+                <h3 style="margin-bottom: 0;">{{this.item.title}}</h3>
+                <strong>{{this.item.uploader}}</strong>
+                <p style="margin-top: 10px">{{this.item.description}}</p>
+            </div>
+        </div>
+    </main>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+    name: "Watch",
+    mounted(){
+        axios.get(`/items/${this.$route.params.id}`)
+        .then(result => {
+            this.item = result.data;
+        });
+    },
+    data(){
+        return({
+            item: null
+        });
+    },
+    computed:{
+        serverUrl(){
+            return process.env.VUE_APP_SERVER_ADDRESS;
+        }
+    }
+}
+</script>
+<style scoped>
+header
+{
+    padding: 10px;
+    position: fixed;
+    background: #34495e;
+    width: 100%;
+    top: 0;
+    min-height: 50px;
+}
+img
+{
+    height: 25px !important;
+    margin-right: 5px;
+}
+video
+{
+    width: 1024px;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+}
+</style>
