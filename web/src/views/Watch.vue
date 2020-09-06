dc27763847e0892118c41600cbdb19213d90812f
Enrique <enrique.kreuk@icloud.com>
2020-09-06 09:34:58 +0200

    Added svg to the template

--- web/src/views/Watch.vue
+++ web/src/views/Watch.vue
@@ -2,7 +2,11 @@
     <main>
         <header>
             <router-link :to="{name:'Home'}">
-                <img src="@/assets/icons/arrowleft.svg" alt="Back" />
+                <!-- <img src="@/assets/icons/arrowleft.svg" alt="Back" /> -->
+                <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                    width="512px" height="512px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512; width: 25px; height: 25px; fill:#FFF" xml:space="preserve">
+                    <polygon points="64.5,256.5 256.5,448.5 256.5,336.5 448.5,336.5 448.5,176.5 256.5,176.5 256.5,64.5 "/>
+                </svg>
             </router-link>
         </header>
         <div v-if="this.item" class="d-flex justify-content-center" style="margin-top: 80px">
@@ -18,6 +22,7 @@
 </template>
 <script>
 import axios from 'axios';
+import arrowLeft from '@/assets/icons/arrowleft.svg';
 
 export default {
     name: "Watch",
@@ -29,7 +34,8 @@ export default {
     },
     data(){
         return({
-            item: null
+            item: null,
+            arrowLeftIcon: arrowLeft
         });
     }
 }
