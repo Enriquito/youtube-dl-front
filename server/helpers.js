bef82a36603ec5c28f071c5a428905b8166664c5
Enrique <enrique.kreuk@icloud.com>
2020-09-09 20:46:26 +0200

    Added reject return

--- server/helpers.js
+++ server/helpers.js
@@ -5,20 +5,21 @@ module.exports.writeDatabase = (data) => {
 
         fs.writeFile(`../config/database.json`, JSON.stringify(data), (error) => {
             if(error){
-                  reject(error);
+                reject(error);
+                return;
             }
 
             resolve(true);
-
-            return;
       });
     });
 }
 module.exports.readDatabase = () => {
     return new Promise((resolve,reject) => {
         fs.readFile('../config/database.json', "utf8", function (error,data) {
-            if (error)
-                  reject(error);
+            if(error){
+                reject(error);
+                return;
+            }
 
             resolve(JSON.parse(data));
       });
