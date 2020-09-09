d2f5eebf19be92c48f5d9e41cf34d8b2dbe4b6e6
Enrique <enrique.kreuk@icloud.com>
2020-09-09 20:46:49 +0200

    Created downloaditem component

---
+++ web/src/components/DownloadItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="item-holder">
+        <span>{{title}}</span>
+        <progress :value="progressValue" max="100"></progress>
+    </div>
+</template>
+<script>
+export default {
+    name: "DownloadManager",
+    props:{
+        title: String,
+        progressValue: Number
+    }
+}
+</script>
+<style scoped>
+@keyframes slideIn{
+    0%{
+        position: relative;
+        left: -328px;
+    }
+    100%{
+        left: auto;
+    }
+}
+.item-holder{
+    animation: slideIn forwards 0.5s;
+    translate: all;
+}
+progress {
+    padding: 10px;
+    width: 100%;
+}
+span{
+    display: block;
+    max-width: 230px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+}
+</style>
