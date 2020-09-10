fe777e2f9f104b26c59d6c0899e18ab3e1606e9d
Enrique Kreuk <Enrique.Kreuk@zuiderzeemuseum.nl>
2020-09-10 13:29:18 +0200

    Added functions in a try catch block

--- server/helpers.js
+++ server/helpers.js
@@ -2,26 +2,37 @@ const fs = require('fs')
 
 module.exports.writeDatabase = (data) => {
     return new Promise((resolve,reject) => {
-
-        fs.writeFile(`../config/database.json`, JSON.stringify(data), (error) => {
-            if(error){
-                reject(error);
-                return;
-            }
-
-            resolve(true);
-      });
+        try{
+            fs.writeFile(`../config/database.json`, JSON.stringify(data), (error) => {
+                if(error){
+                    reject(error);
+                    return;
+                }
+    
+                resolve(true);
+          });
+        }
+        catch(error){
+            console.log(error);
+            reject(error);
+        }
     });
 }
 module.exports.readDatabase = () => {
     return new Promise((resolve,reject) => {
-        fs.readFile('../config/database.json', "utf8", function (error,data) {
-            if(error){
-                reject(error);
-                return;
-            }
-
-            resolve(JSON.parse(data));
-      });
+        try{
+            fs.readFile('../config/database.json', "utf8", function (error,data) {
+                if(error){
+                    reject(error);
+                    return;
+                }
+    
+                resolve(JSON.parse(data));
+          });
+        }
+        catch(error){
+            console.log(error);
+            reject(error);
+        }
     });
 }
