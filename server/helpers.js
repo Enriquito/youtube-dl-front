6cefba9030d34cbc97c0b03b607fe3e14eba71d5
Enrique <enrique.kreuk@icloud.com>
2020-09-12 14:04:17 +0200

    Added helpers for settings data

--- server/helpers.js
+++ server/helpers.js
@@ -8,7 +8,7 @@ module.exports.writeDatabase = (data) => {
                     reject(error);
                     return;
                 }
-    
+
                 resolve(true);
           });
         }
@@ -26,7 +26,44 @@ module.exports.readDatabase = () => {
                     reject(error);
                     return;
                 }
-    
+
+                resolve(JSON.parse(data));
+          });
+        }
+        catch(error){
+            console.log(error);
+            reject(error);
+        }
+    });
+}
+
+module.exports.writeSettings = (data) => {
+    return new Promise((resolve,reject) => {
+        try{
+            fs.writeFile(`./settings.json`, JSON.stringify(data), (error) => {
+                if(error){
+                    reject(error);
+                    return;
+                }
+
+                resolve(true);
+          });
+        }
+        catch(error){
+            console.log(error);
+            reject(error);
+        }
+    });
+}
+module.exports.readSettings = () => {
+    return new Promise((resolve,reject) => {
+        try{
+            fs.readFile('./settings.json', "utf8", function (error,data) {
+                if(error){
+                    reject(error);
+                    return;
+                }
+
                 resolve(JSON.parse(data));
           });
         }
