012ffe1946b425c896a12700bf60a3e563e4988a
Enrique <enrique.kreuk@icloud.com>
2020-09-12 14:05:09 +0200

    Added settingsOpen variable

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -6,11 +6,15 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     downloadOpen: true,
+    settingsOpen: false,
     isDownloading: false
   },
   mutations: {
     downloadOpen(state, value) {
       state.downloadOpen = value;
+    },
+    settingsOpen(state, value) {
+      state.settingsOpen = value;
     },
     isDownloading(state, value){
         state.isDownloading = value;
@@ -19,6 +23,9 @@ export default new Vuex.Store({
   getters: {
     downloadOpen: state => {
       return state.downloadOpen;
+    },
+    settingsOpen: state => {
+      return state.settingsOpen;
     },
     isDownloading: state => {
         return state.isDownloading;
