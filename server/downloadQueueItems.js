99fdee63e60316bad941397d0e1b313e9e06eaa7
Enrique <enrique.kreuk@icloud.com>
2020-09-20 14:06:48 +0200

    Function to download queued items

---
+++ server/downloadQueueItems.js
@@ -0,0 +1,26 @@
+const Media = require('./media');
+const {readDatabase} = require('./helpers');
+
+
+const downloadQueueItems = async () =>{
+    const db = await readDatabase();
+
+    db.downloads.forEach(async el => {
+
+    });
+
+    for(let i = 0; i < db.downloads.length; i++){
+        let el = db.downloads[i];
+
+        if(el.status === 'queued'){
+            const media = new Media();
+
+            media.url = el.url;
+            media.options = el.options;
+
+            await media.Download();
+        }
+    }
+}
+
+downloadQueueItems();
