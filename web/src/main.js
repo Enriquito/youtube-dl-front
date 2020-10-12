e248d10d766dfc67d3d0593bd9d2750a9a9ff767
Enrique <enrique.kreuk@icloud.com>
2020-10-12 20:33:06 +0200

    Changed url

--- web/src/main.js
+++ web/src/main.js
@@ -6,7 +6,7 @@ import VueSocketIO from 'vue-socket.io'
 
 Vue.use(new VueSocketIO({
   debug: true,
-  connection: 'http://localhost:3000',
+  connection: 'localhost:3000',
 }))
 
 Vue.config.productionTip = false
