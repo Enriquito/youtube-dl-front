09cc2467f48f6f051c9271df652ce6dc629b8ee0
Enrique <enrique.kreuk@icloud.com>
2022-01-26 12:37:15 +0100

    added run, get and all methods

--- server/database.js
+++ server/database.js
@@ -10,13 +10,22 @@ class Database {
 				  return;
 				}
 			
-				console.log('Connected to the database.');
+				// console.log('Connected to the database.');
 				
 				resolve(database);
 			});
 		});
 		
 		return db;
+	}
+
+	static close(db){
+		db.close((err) => {
+			if (err) {
+			  return console.error(err.message);
+			}
+			// console.log('Closed the database connection.');
+		});
 	}
 
 	static async checkFirstUse(){
@@ -28,10 +37,9 @@ class Database {
 
 				const isFirstUse = await Database.isFirstUse(db);
 
-				if(isFirstUse){
+				if(isFirstUse)
 					await Database.createTables(db);
-				}
-
+				
 				resolve();
 			}
 			catch(error){
@@ -64,153 +72,208 @@ class Database {
 	}
 
 	static async createTables(db){
-		console.log("Creating tables...");
+		return new Promise(async (resolve, reject) => {
+			console.log("Creating tables...");
 
-		const createSettingsTable = `
-			CREATE TABLE settings (
-			id INTEGER NOT NULL,
-			port INTEGER NOT NULL,
-			default_quality TEXT NOT NULL,
-			output_location TEXT NOT NULL,
-			authentication_username TEXT,
-			authentication_password TEXT,
-			authentication_2fa INTEGER
-		)`;
+			const createSettingsTable = `
+				CREATE TABLE settings (
+				id INTEGER NOT NULL,
+				port INTEGER NOT NULL,
+				default_quality TEXT NOT NULL,
+				output_location TEXT NOT NULL,
+				authentication_username TEXT,
+				authentication_password TEXT,
+				authentication_2fa INTEGER
+			)`;
 
-		const createSettingsValues = `
-			INSERT INTO settings (id, port, default_quality, output_location)
-			VALUES(1, 3000, '720p', './videos')
-		`;
-
-		const createDownloadTable = `
-			CREATE TABLE "downloads" (
-				"id"	INTEGER,
-				"video_id"	TEXT NOT NULL,
-				"title"	TEXT NOT NULL,
-				"status"	TEXT NOT NULL,
-				"process_id"	INTEGER NOT NULL,
-				"url"	TEXT NOT NULL,
-				"format"	TEXT NOT NULL,
-				"audio_format"	TEXT NOT NULL,
-				"audio_only"	NUMERIC NOT NULL,
-				"playlist"	NUMERIC NOT NULL,
-				"download_status" INTEGER NOT NULL,
-				PRIMARY KEY("id" AUTOINCREMENT)
-			);
-		`;
-
-		const createTagTable = `
-			CREATE TABLE tags (
-			id INTEGER PRIMARY KEY AUTOINCREMENT, 
-			tag TEXT NOT NULL
-			);`;
-
-		const createVideosTable = `
-			CREATE TABLE videos (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			title TEXT NOT NULL,
-			uploader_url TEXT NOT NULL,
-			view_count bigINTEGER  NOT NULL,
-			duration mediumINTEGER NOT NULL,
-			file_extention TEXT NOT NULL,
-			file_location TEXT NOT NULL,
-			file_name TEXT NOT NULL,
-			video_url TEXT NOT NULL,
-			video_provider_id TEXT NOT NULL,
-			uploader_name TEXT NOT NULL,
-			description TEXT NOT NULL
-			);`;
-		
-		const createTagLinksTable = `
-			CREATE TABLE tag_links (
-			video INTEGER  NOT NULL,
-			tag INTEGER  NOT NULL
-			);`;
-
-		const createThumbnailTable = `
-			CREATE TABLE thumbnails (
-			id INTEGER PRIMARY KEY AUTOINCREMENT, 
-			video INTEGER NOT NULL,
-			height INTEGER NOT NULL,
-			width INTEGER NOT NULL,
-			resolution TEXT NOT NULL,
-			url TEXT NOT NULL
-			);`;
-
-		const alterTables = `
-			BEGIN TRANSACTION;
-
-			ALTER TABLE tags
-			ADD PRIMARY KEY (id);
-
-			ALTER TABLE tag_links
-			ADD KEY video (video),
-			ADD KEY tag (tag);
-
-			ALTER TABLE videos
-			ADD PRIMARY KEY (id);
-
-			ALTER TABLE thumbnails;
-			ADD KEY video (video);
-
-			ALTER TABLE tag_links
-			ADD CONSTRAINT tag_links_ibfk_1 FOREIGN KEY (video) REFERENCES videos (id),
-			ADD CONSTRAINT tag_links_ibfk_2 FOREIGN KEY (tag) REFERENCES tags (id);
-
-			COMMIT;
+			const createSettingsValues = `
+				INSERT INTO settings (id, port, default_quality, output_location)
+				VALUES(1, 3000, '720p', './videos')
 			`;
-		
-		db.serialize(() => {
-			db
-			.run(createSettingsTable)
-			.run(createSettingsValues)
-			.run(createDownloadTable)
-			.run(createVideosTable)
-			.run(createTagTable)
-			.run(createTagLinksTable)
-			.run(createThumbnailTable)
-			.run(alterTables)
-		})
 
-		console.log("Tables created");
-	}
+			const createDownloadTable = `
+				CREATE TABLE "downloads" (
+					"id"	INTEGER,
+					"video_id"	TEXT NOT NULL,
+					"title"	TEXT NOT NULL,
+					"status"	TEXT NOT NULL,
+					"process_id"	INTEGER,
+					"url"	TEXT NOT NULL,
+					"format"	TEXT NOT NULL,
+					"audio_format"	TEXT,
+					"audio_only"	NUMERIC NOT NULL,
+					"playlist"	NUMERIC NOT NULL,
+					"download_status" INTEGER,
+					PRIMARY KEY("id" AUTOINCREMENT)
+				);
+			`;
 
-	static close(db){
-		db.close((err) => {
-			if (err) {
-			  return console.error(err.message);
+			const createTagTable = `
+				CREATE TABLE tags (
+				id INTEGER PRIMARY KEY AUTOINCREMENT, 
+				tag TEXT NOT NULL
+				);`;
+
+			const createVideosTable = `
+				CREATE TABLE videos (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				title TEXT NOT NULL,
+				uploader_url TEXT NOT NULL,
+				view_count bigINTEGER  NOT NULL,
+				duration mediumINTEGER NOT NULL,
+				file_extention TEXT NOT NULL,
+				file_location TEXT NOT NULL,
+				file_name TEXT NOT NULL,
+				video_url TEXT NOT NULL,
+				video_provider_id TEXT NOT NULL,
+				uploader_name TEXT NOT NULL,
+				description TEXT NOT NULL
+				);`;
+			
+			const createTagLinksTable = `
+				CREATE TABLE tag_links (
+				video INTEGER  NOT NULL,
+				tag INTEGER  NOT NULL
+				);`;
+
+			const createThumbnailTable = `
+				CREATE TABLE thumbnails (
+				id INTEGER PRIMARY KEY AUTOINCREMENT, 
+				video INTEGER NOT NULL,
+				height INTEGER NOT NULL,
+				width INTEGER NOT NULL,
+				resolution TEXT NOT NULL,
+				url TEXT NOT NULL
+				);`;
+
+			const alterTables = `
+				BEGIN TRANSACTION;
+
+				ALTER TABLE tags
+				ADD PRIMARY KEY (id);
+
+				ALTER TABLE tag_links
+				ADD KEY video (video),
+				ADD KEY tag (tag);
+
+				ALTER TABLE videos
+				ADD PRIMARY KEY (id);
+
+				ALTER TABLE thumbnails;
+				ADD KEY video (video);
+
+				ALTER TABLE tag_links
+				ADD CONSTRAINT tag_links_ibfk_1 FOREIGN KEY (video) REFERENCES videos (id),
+				ADD CONSTRAINT tag_links_ibfk_2 FOREIGN KEY (tag) REFERENCES tags (id);
+
+				COMMIT;
+				`;
+
+			try{
+				await Database.run(createSettingsTable);
+				await Database.run(createSettingsValues);
+				await Database.run(createDownloadTable);
+				await Database.run(createVideosTable);
+				await Database.run(createTagTable);
+				await Database.run(createTagLinksTable);
+				await Database.run(createThumbnailTable);
+				await Database.run(alterTables);
+
+				console.log("Tables created");
+				resolve();
 			}
-			console.log('Closed the database connection.');
+			catch(error){
+				console.error(error);
+				reject(error);
+			}
 		});
 	}
 
 	static purge(){
-		return new Promise((resolve, reject) => {
+		return new Promise(async (resolve, reject) => {
 			const videosTable = "DELETE FROM videos";
 			const tagsTable = "DELETE FROM tags";
 			const tagLinksTable = "DELETE FROM tag_links";
 			const thumbnailsTable = "DELETE FROM thumbnails";
 
-			this.database.serialize((error) => {
-				if(error)
-					reject();
+			try{
+				await Database.run(videosTable);
+				await Database.run(tagsTable);
+				await Database.run(tagLinksTable);
+				await Database.run(thumbnailsTable);
 
-				this.database
-				.run(videosTable)
-				.run(tagsTable)
-				.run(tagLinksTable)
-				.run(thumbnailsTable)
-			});
+				console.log("Tables have been cleared.");
+				resolve();
+			}
+			catch(error){
+				console.error(error);
+				reject(error);
+			}
+		});
+	}
 
-			console.log("Tables have been cleared.");
+	static run(query, values){
+		return new Promise(async (resolve,reject) => {
+			let db;
 
-			this.close();
+			try{
+				db = await Database.connect();
 
-			resolve();
+				db.run(query, values, function(error) {
+					if(error) reject(error);
+					else resolve({status: "success", data: this});
+				});
+			}
+			catch(error){
+				console.err(error);
+			}
+			finally{
+				Database.close(db);
+			}
+		});
+	}
+
+	static get(query, values){
+		return new Promise(async (resolve,reject) => {
+			let db;
+
+			try{
+				db = await Database.connect();
+
+				db.get(query, values, function(error,row) {
+					if(error) reject(error);
+					else resolve({status: "success", data: row});
+				});
+			}
+			catch(error){
+				console.err(error);
+			}
+			finally{
+				Database.close(db);
+			}
+		});
+	}
+
+	static all(query, values){
+		return new Promise(async (resolve,reject) => {
+			let db;
+
+			try{
+				db = await Database.connect();
+
+				db.all(query, values, function(error,rows) {
+					if(error) reject(error);
+					else resolve({status: "success", data: rows});
+				});
+			}
+			catch(error){
+				console.err(error);
+			}
+			finally{
+				Database.close(db);
+			}
 		});
 	}
 }
-
-const db = new Database();
 
 module.exports = Database;
