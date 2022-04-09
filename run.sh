0de6b30bb23448b3e94452c9b21921a890057492
Enrique <enrique.kreuk@icloud.com>
2022-04-09 19:47:54 +0200

    Removed lagacy json db checks

--- run.sh
+++ run.sh
@@ -4,20 +4,6 @@
 curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 chmod a+rx /usr/local/bin/youtube-dl
 
-# Check if database file exists
-if [ ! -f "/youtube-dl-front/config/database.json" ];
-then
-    echo "Creating database file"
-    echo "{\"videos\":[],\"downloads\":[]}" >> "/youtube-dl-front/config/database.json"
-fi
-
-# Check if settings file exists
-if [ ! -f "/youtube-dl-front/config/settings.json" ];
-then
-    echo "Creating settings file"
-    echo "{\"port\": 3000,\"defaultQuality\": \"720p\",\"outputLocation\": \"./videos\"}" >> "/youtube-dl-front/config/settings.json"
-fi
-
 # Change to the server directory and run the application
 cd /youtube-dl-front/server
 node app.js
